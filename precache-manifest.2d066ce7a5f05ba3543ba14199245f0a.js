8f35cb176e00fb55150917eb4caab629656ec0d9
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2020-06-28 17:57:45 +0530

    Updates

---
+++ precache-manifest.2d066ce7a5f05ba3543ba14199245f0a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc7c03a619d29d945a43a832481dd5c",
+    "url": "/Apptitude/index.html"
+  },
+  {
+    "revision": "3398e6b0b441ade81d5c",
+    "url": "/Apptitude/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8219520d124e9f487166",
+    "url": "/Apptitude/static/css/main.8b92e52b.chunk.css"
+  },
+  {
+    "revision": "3398e6b0b441ade81d5c",
+    "url": "/Apptitude/static/js/2.5534c1a4.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/Apptitude/static/js/2.5534c1a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8219520d124e9f487166",
+    "url": "/Apptitude/static/js/main.cc326854.chunk.js"
+  },
+  {
+    "revision": "dc8b9a3628b1c5f681fd",
+    "url": "/Apptitude/static/js/runtime-main.de88134a.js"
+  },
+  {
+    "revision": "4180e23f787abc8ec458f9224ffebea1",
+    "url": "/Apptitude/static/media/CerebriSans-Bold.4180e23f.ttf"
+  },
+  {
+    "revision": "43a5f55cc9a045dada7f83636531d112",
+    "url": "/Apptitude/static/media/CerebriSans-Bold.43a5f55c.eot"
+  },
+  {
+    "revision": "18af979faeba9fa69e65cd3f4dfe7d19",
+    "url": "/Apptitude/static/media/CerebriSans-ExtraBold.18af979f.eot"
+  },
+  {
+    "revision": "562a9e390c4c081cc810b025ce0dc919",
+    "url": "/Apptitude/static/media/CerebriSans-ExtraBold.562a9e39.ttf"
+  },
+  {
+    "revision": "03fd61fc4580b10075a16514220381d3",
+    "url": "/Apptitude/static/media/CerebriSans-Light.03fd61fc.ttf"
+  },
+  {
+    "revision": "78c6363053ccaebd166e34349fac8cc9",
+    "url": "/Apptitude/static/media/CerebriSans-Light.78c63630.eot"
+  },
+  {
+    "revision": "91bff5f0d5f1128401a41dc3269a684b",
+    "url": "/Apptitude/static/media/CerebriSans-Regular.91bff5f0.ttf"
+  },
+  {
+    "revision": "e63b280fde60f13c59b2eab17adfe03a",
+    "url": "/Apptitude/static/media/CerebriSans-Regular.e63b280f.eot"
+  },
+  {
+    "revision": "25aa37eee4a8a72fa7d2702b548cd843",
+    "url": "/Apptitude/static/media/CerebriSans-SemiBold.25aa37ee.ttf"
+  },
+  {
+    "revision": "2a1663dd85b07223f419fc0e9b5abb53",
+    "url": "/Apptitude/static/media/CerebriSans-SemiBold.2a1663dd.eot"
+  },
+  {
+    "revision": "70deea49a040db982444046465017189",
+    "url": "/Apptitude/static/media/FacebookIcon.70deea49.svg"
+  },
+  {
+    "revision": "0ae521bfe45ff66bd34046d68980f77d",
+    "url": "/Apptitude/static/media/LinkedinIcon.0ae521bf.svg"
+  },
+  {
+    "revision": "7cca4a5ed6dae767a1026bcb05dcefff",
+    "url": "/Apptitude/static/media/instaIcon.7cca4a5e.svg"
+  }
+]);
