28b5e5ad38f5d078f9fdf640828587769055eae7
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2020-06-29 14:18:19 +0530

    added userAuth

--- src/FormInput.js
+++ src/FormInput.js
@@ -29,7 +29,7 @@ class FormInput extends React.Component {
                 this.setState({
                   exists: "true",
                 });
-              }
+            }
             });
           })
           .then((data) => {
